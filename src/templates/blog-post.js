9a42854eb8e67a7f2fded94a6f281f6701de319f
Sarah German <sarah@sarahgerman.com>
2020-02-23 12:09:38 -0600

    Add tag pages

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,21 +1,27 @@
 import React from "react"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
+import SEO from "../components/seo"
 
 export default ({ data }) => {
   const post = data.allWordpressPost.edges[0].node
   let tagList = [];
+  // @todo use <link> tags
   post.tags.forEach((tag) => {
-    tagList.push(tag.name)
+    tagList.push(`<a href="/tag/${tag.slug}">` + tag.name + '</a>')
   });
 
   return (
     <Layout>
+      <SEO title={post.title} />
       <div>
         <h1 dangerouslySetInnerHTML={{ __html: post.title }} />
         <h4 className="byline">Posted by {post.author.name} on {post.date}</h4>
         <div dangerouslySetInnerHTML={{ __html: post.content }} />
-        <div className="tags">Tagged: {tagList.join(', ')}</div>
+        <hr/>
+        <div className="tags">Tagged:&nbsp;
+          <span dangerouslySetInnerHTML={{ __html: tagList.join(', ') }} />
+        </div>
       </div>
     </Layout>
   )
---
+++ src/templates/tag.js
@@ -0,0 +1,54 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+export default ({ data, pageContext }) => {
+  console.log(pageContext);
+  return (
+    <Layout>
+      <SEO title={pageContext.name} />
+      <h2>Posts tagged {pageContext.name}</h2>
+      {data.allWordpressPost.edges.map(({ node }) => (
+        <div
+          key={node.slug}
+          style={{
+            borderBottom: `1px dotted gray`,
+            marginBottom: `2em`,
+          }}
+        >
+          <h3 style={{ marginBottom: `.25rem` }}>
+            <Link to={node.slug}>
+              <span dangerouslySetInnerHTML={{ __html: node.title }} />
+            </Link>
+          </h3>
+          <h4 className="byline">
+            Posted by {node.author.name} on {node.date}
+          </h4>
+          <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+        </div>
+      ))}
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query($slug: String!) {
+      allWordpressPost(filter: {tags: {elemMatch: {slug: {eq: $slug}}}}) {
+        edges {
+          node {
+            title
+            content
+            date(formatString: "MMMM Do, YYYY")
+            tags {
+              name
+              slug
+            }
+            author {
+              name
+            }
+          }
+        }
+      }
+  }
+ `
