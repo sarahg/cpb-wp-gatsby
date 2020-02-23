3ad1f0e58ca8c24f983055a35c2efd61ca177ad4
Sarah German <sarah@sarahgerman.com>
2020-02-23 09:46:57 -0600

    Add tags, date and author info to posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,11 +4,18 @@ import { graphql } from "gatsby"
 
 export default ({ data }) => {
   const post = data.allWordpressPost.edges[0].node
+  let tagList = [];
+  post.tags.forEach((tag) => {
+    tagList.push(tag.name)
+  });
+
   return (
     <Layout>
       <div>
         <h1 dangerouslySetInnerHTML={{ __html: post.title }} />
+        <h4 className="byline">Posted by {post.author.name} on {post.date}</h4>
         <div dangerouslySetInnerHTML={{ __html: post.content }} />
+        <div className="tags">Tagged: {tagList.join(', ')}</div>
       </div>
     </Layout>
   )
@@ -20,6 +27,17 @@ export const query = graphql`
         node {
           title
           content
+          date(formatString: "MMMM Do, YYYY")
+          tags {
+            name
+            slug
+          }
+          author {
+            name
+            avatar_urls {
+              wordpress_48
+            }
+          }
         }
       }
     }
